65dd4dd965bdd68dc5c287921d16265ae96e1d66
Kieran Graham <me@kierangraham.com>
2011-10-04 20:11:52 +0100

    still fixing the random() thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,8 @@ class ApplicationController < ActionController::Base
                             :limit => 3,
                             :order => 'event_date ASC',
                             :conditions => "event_date >= DATE(NOW())")
-    STDERR.puts "FUUUUCCCCCCKKKKKKKK -> #{RAILS_ENV}"
-    if (RAILS_ENV == :production)
+
+    if (RAILS_ENV.to_sym == :production)
       @rcphoto = Photo.find(:first, :order => 'random()')
     else
       @rcphoto = Photo.find(:first, :order => 'RAND()')
